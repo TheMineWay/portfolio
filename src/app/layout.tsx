e6dee129e4390aae98d734fafac08aac2d507503
TheMineWay <joelcamposoliva9000@gmail.com>
2024-02-23 18:59:09 +0100

    Installed UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { PrimeReactProvider } from "primereact/api";
+import "primereact/resources/themes/lara-dark-purple/theme.css";
 import "./globals.pcss";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,7 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <PrimeReactProvider>{children}</PrimeReactProvider>
+      </body>
     </html>
   );
 }
