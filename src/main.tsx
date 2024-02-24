c3488b10b0b272af13ee801f0bd268bc2105864f
TheMineWay <joelcamposoliva9000@gmail.com>
2024-02-24 10:36:43 +0100

    Import theme

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Providers from "@providers/providers";
 import Title from "@components/presentation/title";
+import "primereact/resources/themes/lara-dark-purple/theme.css";
 import "./index.pcss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
