8eb7cbf252d8e4d1abcceee5b98210819a161334
TheMineWay <joelcamposoliva9000@gmail.com>
2024-02-24 10:47:08 +0100

    i18n

--- src/components/presentation/title.tsx
+++ src/components/presentation/title.tsx
@@ -1,9 +1,13 @@
+import { useTranslation } from "react-i18next";
 import styles from "./title.module.pcss";
+import { Translation } from "@i18n/translation.enum";
 
 export default function Title() {
+  const { t } = useTranslation([Translation.PRESENTATION]);
+
   return (
     <div className={styles.title} role="heading">
-      <p>Hi, my name is</p>
+      <p>{t("title.Name")}</p>
       <h1>Joel Campos</h1>
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import Providers from "@providers/providers";
 import Title from "@components/presentation/title";
 import "primereact/resources/themes/lara-dark-purple/theme.css";
 import "./index.pcss";
+import { i18nSetup } from "@i18n/i18n.setup";
+
+i18nSetup();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
