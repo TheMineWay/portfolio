c948bf1ec66db6d6be624b1318a32dd6d9016b57
TheMineWay <joelcamposoliva9000@gmail.com>
2024-02-24 04:01:06 +0100

    Change message

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import "./index.pcss";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Providers>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">Init</h1>
     </Providers>
   </React.StrictMode>
 );
