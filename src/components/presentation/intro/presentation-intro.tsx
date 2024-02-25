38668b85bea4021afebfd8d59ac4290a2387fb44
TheMineWay <joelcamposoliva9000@gmail.com>
2024-02-25 10:28:04 +0100

    Add QR code

--- src/components/presentation/intro/presentation-intro.tsx
+++ src/components/presentation/intro/presentation-intro.tsx
@@ -1,6 +1,8 @@
 import { useTranslation } from "react-i18next";
 import Title from "./title";
 import { Translation } from "@i18n/translation.enum";
+import { useState } from "react";
+import QRCode from "react-qr-code";
 import styles from "./presentation-intro.module.pcss";
 
 import meImage from "@assets/me/me.jpg";
@@ -10,12 +12,27 @@ import github from "@assets/social/github.png";
 export default function PresentationIntro() {
   const { t } = useTranslation([Translation.PRESENTATION]);
 
+  const [isQr, setIsQr] = useState(false);
+
   return (
     <div className="grid content-between gap-12 h-full px-4">
       <span></span>
       <div className="flex flex-col lg:flex-row justify-center items-center gap-12 lg:gap-24">
         <Title />
-        <img alt={t("intro.image.Alt")} src={meImage} className={styles.me} />
+        <div className={styles.cover}>
+          {isQr ? (
+            <QRCode
+              onDoubleClick={() => setIsQr(false)}
+              value={window.location.href}
+            />
+          ) : (
+            <img
+              onDoubleClick={() => setIsQr(true)}
+              alt={t("intro.image.Alt")}
+              src={meImage}
+            />
+          )}
+        </div>
       </div>
       <div
         className={`px-4 pb-4 justify-self-end flex justify-center gap-4 ${styles.social}`}
