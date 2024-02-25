de3de0ba1a788a10672dd780b8e7a644b7fafab9
TheMineWay <joelcamposoliva9000@gmail.com>
2024-02-25 12:48:58 +0100

    Some clouds

--- src/components/presentation/about-me/about-me-quote.tsx
+++ src/components/presentation/about-me/about-me-quote.tsx
@@ -2,22 +2,40 @@ import { useTranslation } from "react-i18next";
 import { Translation } from "@i18n/translation.enum";
 import styles from "./about-me-quote.module.pcss";
 
-import beer from "@assets/beer.png";
-
 type Props = {
   quote: string;
+  icon: {
+    src: string;
+    alt: string;
+  };
+  offset: {
+    x: number;
+    y: number;
+    right?: boolean;
+  };
 };
 
-export default function AboutMeQuote({ quote }: Props) {
+export default function AboutMeQuote({
+  quote,
+  icon: { src: iconSrc, alt: iconAlt },
+  offset: { x: xOffset, y: yOffset, right },
+}: Props) {
   const { t } = useTranslation([Translation.PRESENTATION]);
 
   return (
-    <div className={styles.cloud} style={{ top: "5em", left: "5em" }}>
+    <div
+      className={styles.cloud}
+      style={{
+        top: `${yOffset}em`,
+        left: right ? undefined : `${xOffset}em`,
+        right: right ? `${xOffset}em` : undefined,
+      }}
+    >
       <div className={styles.card}>
         <div className={styles.content}>
           <p>{t(`about-me.quotes.${quote}.Text`)}</p>
         </div>
-        <img src={beer} alt="Beer" />
+        <img src={iconSrc} alt={iconAlt} />
       </div>
     </div>
   );
--- src/components/presentation/about-me/about-me.tsx
+++ src/components/presentation/about-me/about-me.tsx
@@ -1,9 +1,38 @@
 import AboutMeQuote from "./about-me-quote";
 
+import beer from "@assets/beer.png";
+
 export default function AboutMe() {
+  const icon = {
+    src: beer,
+    alt: "Beer",
+  };
+
   return (
     <div className="w-full h-full relative">
-      <AboutMeQuote quote="1" />
+      <AboutMeQuote quote="1" icon={icon} offset={{ x: 10, y: -40 }} />
+      <AboutMeQuote quote="2" icon={icon} offset={{ x: 35, y: -20 }} />
+      <AboutMeQuote quote="3" icon={icon} offset={{ x: 25, y: 0 }} />
+      <AboutMeQuote
+        quote="4"
+        icon={icon}
+        offset={{ x: 15, y: -30, right: true }}
+      />
+      <AboutMeQuote
+        quote="5"
+        icon={icon}
+        offset={{ x: 25, y: -5, right: true }}
+      />
+      <AboutMeQuote
+        quote="6"
+        icon={icon}
+        offset={{ x: 60, y: 0, right: true }}
+      />
+      <AboutMeQuote
+        quote="7"
+        icon={icon}
+        offset={{ x: 40, y: 15, right: true }}
+      />
     </div>
   );
 }
