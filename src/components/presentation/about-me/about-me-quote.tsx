d47f0b6f5eac346e38d30a158c1a8c895a8c0c13
TheMineWay <joelcamposoliva9000@gmail.com>
2024-02-25 16:49:36 +0100

    About me almost ended

--- src/components/presentation/about-me/about-me-quote.tsx
+++ src/components/presentation/about-me/about-me-quote.tsx
@@ -1,5 +1,6 @@
 import { useTranslation } from "react-i18next";
 import { Translation } from "@i18n/translation.enum";
+import { useScreen } from "@hooks/screen/use-screen";
 import styles from "./about-me-quote.module.pcss";
 
 type Props = {
@@ -10,25 +11,34 @@ type Props = {
   };
   offset: {
     x: number;
-    y: number;
+    y?: number;
     right?: boolean;
   };
+  index: number;
 };
 
 export default function AboutMeQuote({
   quote,
   icon: { src: iconSrc, alt: iconAlt },
-  offset: { x: xOffset, y: yOffset, right },
+  offset: { x: xOffset, y: yOffset, right: isRight },
+  index,
 }: Props) {
   const { t } = useTranslation([Translation.PRESENTATION]);
+
+  const { isMobile } = useScreen();
+
+  const left = isRight ? undefined : `${xOffset}%`;
+  const right = isRight ? `${xOffset}%` : undefined;
+
+  const isEven = index % 2 === 0;
 
   return (
     <div
       className={styles.cloud}
       style={{
-        top: `${yOffset}em`,
-        left: right ? undefined : `${xOffset}em`,
-        right: right ? `${xOffset}em` : undefined,
+        top: `${(yOffset ?? 0) + index * 7}em`,
+        left: isMobile ? (isEven ? undefined : "3em") : left,
+        right: isMobile ? (isEven ? "3em" : undefined) : right,
       }}
     >
       <div className={styles.card}>
--- src/components/presentation/about-me/about-me.tsx
+++ src/components/presentation/about-me/about-me.tsx
@@ -1,38 +1,113 @@
 import AboutMeQuote from "./about-me-quote";
 
-import beer from "@assets/beer.png";
+import beer from "@assets/about-me/beer.png";
+import book from "@assets/about-me/book.png";
+import code from "@assets/about-me/code.png";
+import coffee from "@assets/about-me/coffee-cup.png";
+import game from "@assets/about-me/game.png";
+import idea from "@assets/about-me/idea.png";
+import js from "@assets/about-me/js.png";
 
 export default function AboutMe() {
-  const icon = {
-    src: beer,
-    alt: "Beer",
-  };
+  const quotes: {
+    quote: string;
+    icon: {
+      src: string;
+      alt: string;
+    };
+    offset: {
+      x: number;
+      right?: boolean;
+    };
+  }[] = [
+    {
+      quote: "aw",
+      icon: {
+        src: beer,
+        alt: "Beer",
+      },
+      offset: {
+        x: 30,
+      },
+    },
+    {
+      quote: "bootcamp",
+      icon: {
+        src: book,
+        alt: "Book",
+      },
+      offset: {
+        x: 35,
+        right: true,
+      },
+    },
+    {
+      quote: "ht",
+      icon: {
+        src: code,
+        alt: "Code",
+      },
+      offset: {
+        x: 5,
+      },
+    },
+    {
+      quote: "jam",
+      icon: {
+        src: game,
+        alt: "Game",
+      },
+      offset: {
+        x: 15,
+        right: true,
+      },
+    },
+    {
+      quote: "js",
+      icon: {
+        src: js,
+        alt: "JavaScript",
+      },
+      offset: {
+        x: 40,
+        right: true,
+      },
+    },
+    {
+      quote: "idea",
+      icon: {
+        src: idea,
+        alt: "Idea",
+      },
+      offset: {
+        x: 60,
+        right: true,
+      },
+    },
+    {
+      quote: "coffee",
+      icon: {
+        src: coffee,
+        alt: "Coffee",
+      },
+      offset: {
+        x: 25,
+        right: true,
+      },
+    },
+  ];
 
   return (
     <div className="w-full h-full relative">
-      <AboutMeQuote quote="1" icon={icon} offset={{ x: 10, y: -40 }} />
-      <AboutMeQuote quote="2" icon={icon} offset={{ x: 35, y: -20 }} />
-      <AboutMeQuote quote="3" icon={icon} offset={{ x: 25, y: 0 }} />
-      <AboutMeQuote
-        quote="4"
-        icon={icon}
-        offset={{ x: 15, y: -30, right: true }}
-      />
-      <AboutMeQuote
-        quote="5"
-        icon={icon}
-        offset={{ x: 25, y: -5, right: true }}
-      />
-      <AboutMeQuote
-        quote="6"
-        icon={icon}
-        offset={{ x: 60, y: 0, right: true }}
-      />
-      <AboutMeQuote
-        quote="7"
-        icon={icon}
-        offset={{ x: 40, y: 15, right: true }}
-      />
+      {quotes.map(({ icon, offset, quote }, i) => (
+        <AboutMeQuote
+          key={i}
+          quote={quote}
+          icon={icon}
+          offset={offset}
+          index={i}
+        />
+      ))}
     </div>
   );
 }
