9eeb1631c071d7ea71acc41037542521cfcfe045
Joel Campos <joelcamposoliva9000@gmail.com>
2026-03-07 21:56:15 +0100

    feat: :wrench: use site url

--- features/about/lib/get-my-json-ld.ts
+++ features/about/lib/get-my-json-ld.ts
@@ -3,18 +3,16 @@ import { getTranslations } from "next-intl/server";
 import { MY_DETAILS } from "../../../constants/my-details";
 import { SOCIAL } from "@/constants/social.constants";
 import { TOP_TECHNOLOGIES } from "@/constants/technologies.constant";
+import { SITE_URL } from "@/constants/site-url";
 
 export const getMyJsonLd = async () => {
     const t = await getTranslations(TranslationNamespace.ABOUT);
 
-    // TODO: implement current domain
-    const PERSONAL_DOMAIN = "";
-
     return {
         "@type": "Person",
-        "@id": `${PERSONAL_DOMAIN}/#person`,
+        "@id": `${SITE_URL}/#person`,
         "name": MY_DETAILS.name,
-        "url": PERSONAL_DOMAIN,
+        "url": SITE_URL,
         "image": MY_DETAILS.avatar.src,
         "jobTitle": MY_DETAILS.jobTitle,
         "description": t('about-me.About-me'),
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 
-const nextConfig = {};
+if (!process.env.SITE_URL) {
+    throw new Error('Missing required environment variable: SITE_URL');
+}
+
+const nextConfig = {
+    env: {
+        SITE_URL: process.env.SITE_URL,
+    },
+};
 
 export default withNextIntl(nextConfig);
