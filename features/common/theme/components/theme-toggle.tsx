1f566569aedc79928756f9979d20332978789ea3
Joel Campos <joelcamposoliva9000@gmail.com>
2026-03-09 09:15:47 +0100

    feat: :sparkles: theme toggle functionality

--- features/common/theme/components/theme-toggle.tsx
+++ features/common/theme/components/theme-toggle.tsx
@@ -1,15 +1,19 @@
+"use client"
 import { Button } from "@/components/ui/button/button";
 import { TranslationNamespace } from "@/i18n/namespaces";
 import { Moon, Sun } from "lucide-react";
-import { getTranslations } from "next-intl/server";
+import { useTheme } from "../hooks/use-theme";
+import { useTranslations } from "next-intl";
 
-export const ThemeToggle: React.FC = async () => {
-    const t = await getTranslations(TranslationNamespace.COMMON);
+export const ThemeToggle: React.FC = () => {
+    const t = useTranslations(TranslationNamespace.COMMON);
+
+    const { toggle } = useTheme();
 
     return (
-        <Button aria-label={t('components.theme-toggle.Label')} variant='ghost' size='icon'>
+        <Button aria-label={t('components.theme-toggle.Label')} variant='ghost' size='icon' onClick={toggle}>
             <Sun className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"/>
             <Moon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"/>
         </Button>
-    )
+    );
 };
