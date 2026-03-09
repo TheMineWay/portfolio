104d4f1b94ecd807f8bc632debcefe8ed58276ce
Joel Campos <joelcamposoliva9000@gmail.com>
2026-03-09 11:25:52 +0100

    feat: :sparkles: footer social links

--- components/common/layout/footer.tsx
+++ components/common/layout/footer.tsx
@@ -1,8 +1,17 @@
+import { Github } from "@/components/icons/github";
+import { Linkedin } from "@/components/icons/linkedin";
+import { Button } from "@/components/ui/button/button";
+import { SOCIAL } from "@/constants/social.constants";
+import { TranslationNamespace } from "@/i18n/namespaces";
 import { formatDate } from "date-fns";
+import { getTranslations } from "next-intl/server";
+import Link from "next/link";
 
 const CURRENT_YEAR = formatDate(new Date(), "yyyy");
 
-export const Footer: React.FC = () => {
+export const Footer: React.FC = async () => {
+    const t = await getTranslations(TranslationNamespace.COMMON);
+
     return <footer className="py-12 border-t">
         <div className="container mx-auto px-4 sm:px-6 lg:px-8">
             <div className="max-w-6xl mx-auto">
@@ -12,7 +21,14 @@ export const Footer: React.FC = () => {
                     </div>
 
                     <div className="flex items-center gap-4">
-                        {/* LINKS */}
+                        {/* SOCIAL LINKS */}
+                       {SOCIAL_LINKS.map(({ name, link, icon }) => (
+                         <Link href={link} target="_blank" key={name}>
+                            <Button variant="ghost" size="icon" aria-label={t("expressions.Link-to", { name })}>
+                                {icon}
+                            </Button>
+                        </Link>
+                       ))}
                     </div>
                 </div>
 
@@ -23,3 +39,24 @@ export const Footer: React.FC = () => {
         </div>
     </footer>;
 };
+
+/* Content */
+
+type SocialLink = {
+    name: string;
+    link: string;
+    icon: React.ReactNode;
+}
+
+const SOCIAL_LINKS = [
+    {
+        name: "GitHub",
+        link: SOCIAL.github,
+        icon: <Github className="fill-foreground" />
+    },
+    {
+        name: "LinkedIn",
+        link: SOCIAL.linkedin,
+        icon: <Linkedin className="fill-foreground" />
+    }
+];
