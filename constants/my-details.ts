4bd94d5f4234535dfc82eeeeb3434ff0e7495672
Joel Campos <joelcamposoliva9000@gmail.com>
2026-03-09 15:20:34 +0100

    feat: :sparkles: use first name as company metadata title

--- constants/my-details.ts
+++ constants/my-details.ts
@@ -3,6 +3,7 @@ import avatar from "@/assets/avatar.png";
 
 export const MY_DETAILS = {
     fullName: pk.author.name,
+    name: pk.author.name.split(" ")[0],
     avatar,
     jobTitle: "Full-Stack Developer",
 };
