682f70c6d6e5032fcb46529926443cb2f5a38409
Joel Campos <joelcamposoliva9000@gmail.com>
2026-03-09 14:48:09 +0100

    feat: :wrench: static generation & add company links to sitemap

--- app/[locale]/experiences/[company]/page.tsx
+++ app/[locale]/experiences/[company]/page.tsx
@@ -1,5 +1,7 @@
 import { Company } from "@/features/work-experience/constants/companies.constants";
 
+export const dynamic = 'force-static';
+
 export function generateStaticParams() {
   return Object.values(Company).map((company) => ({
     company,
