e97c77dc0f55eac5d0e5208114d9ee46a300dd9f
Joel Campos <joelcamposoliva9000@gmail.com>
2026-03-11 09:44:44 +0100

    fix: :adhesive_bandage: use details in <Header/>

--- components/common/layout/header.tsx
+++ components/common/layout/header.tsx
@@ -1,15 +1,17 @@
 import { ThemeToggle } from "@/features/common/theme/components/theme-toggle";
 import { Link } from "@/i18n/navigation";
 import { Navbar } from "./navbar";
+import { MY_DETAILS } from "@/constants/my-details";
 
 export const Header: React.FC = () => {
-    // TODO: read name from constant
+    const { name, lastName } = MY_DETAILS;
+
     return <header className="sticky top-0 w-full p-2 bg-background/80 backdrop-blur-sm z-50">
         <div className="container mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex h-16 items-center justify-between">
                 <Link href="/" className="text-xl font-semibold hover:opacity-80 transition-opacity">
-                    <span className="text-primary">Joel </span>
-                    <span>Campos</span>
+                    <span className="text-primary">{name} </span>
+                    <span>{lastName}</span>
                 </Link>
                 <Navbar/>
                 <ThemeToggle/>
--- constants/my-details.ts
+++ constants/my-details.ts
@@ -1,9 +1,12 @@
 import pk from "@/package.json";
 import avatar from "@/assets/avatar.png";
 
+const [name, lastName] = pk.author.name.split(" ");
+
 export const MY_DETAILS = {
     fullName: pk.author.name,
-    name: pk.author.name.split(" ")[0],
+    name,
+    lastName,
     avatar,
     jobTitle: "Full-Stack Developer",
 };
